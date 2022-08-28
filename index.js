d2b358c7182ab9c9d3698c0c0badfd031b0c40c9
Dagi <dagi.zewdu.dz@gmail.com>
2022-08-28 08:07:03 +0300

    initiated initial files

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express")
 const cors = require("cors")
+const { port } = require('./config')
 
 const app = express()
 
-app.listen()
+app.listen(port, (err) => {
+    if (err) return console.log(err)
+    console.log(`Server up and running at ${port}`)
+})
